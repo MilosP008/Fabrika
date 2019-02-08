c21bca070b259e37ed9c61cdd2e23c2f0c3048c5
Miloš Petrović <31136523+MilosP008@users.noreply.github.com>
2019-02-08 23:45:24 +0100

    Add files via upload

---
+++ Fabrika/Vozila/Kamioni/CisternaKamion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Vozila.Kamioni
+{
+    class CisternaKamion : Kamion
+    {
+        private double kapacitetTanka;
+
+        public CisternaKamion(double kapacitetTanka, int brojOsovina, string marka, string boja) : base(brojOsovina, marka, boja)
+        {
+            this.kapacitetTanka = kapacitetTanka;
+            zauzima = 20;
+        }
+
+        public override string ToString()
+        {
+            return "Cisterna kamion" + ", marka: " + marka + ", boja: " + boja + ", broj osovina: " + brojOsovina + ", kapacitet tanka: " + kapacitetTanka + ", zauzima: " + zauzima + " m2, parking: " + Parking;
+        }
+    }
+}
---
+++ Fabrika/Vozila/Kamioni/SleperKamion.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Vozila.Kamioni
+{
+    class SleperKamion : Kamion
+    {
+        private int vucnaSnaga;
+
+        public SleperKamion(int vucnaSnaga, int brojOsovina, string marka, string boja) : base(brojOsovina, marka, boja)
+        {
+            this.vucnaSnaga = vucnaSnaga;
+            zauzima = 15;
+        }
+
+        public override string ToString()
+        {
+            return "Sleper kamion" + ", marka: " + marka + ", boja: " + boja + ", broj osovina: " + brojOsovina + ", vucna snaga: " + vucnaSnaga + ", zauzima: " + zauzima + " m2, parking: " + Parking;
+        }
+    }
+}
