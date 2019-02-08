973120d435eb6e3d1e38030ceaf14abafb706931
Miloš Petrović <31136523+MilosP008@users.noreply.github.com>
2019-02-08 23:43:50 +0100

    Add files via upload

---
+++ Fabrika/Vozila/Automobili/KabrioletAutomobil.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Vozila.Automobili
+{
+    class KabrioletAutomobil : Automobil
+    {
+        private bool krovKojiSeMozeSkinuti;
+
+        public KabrioletAutomobil(bool krovKojiSeMozeSkinuti, int brojSedista, string marka, string boja, string[] dodatnaOprema) : base(brojSedista, marka, boja, dodatnaOprema) {
+            this.krovKojiSeMozeSkinuti = krovKojiSeMozeSkinuti;
+        }
+
+        public string GetKrovKojiSeMozeSkinuti {
+            get
+            {
+                if(krovKojiSeMozeSkinuti == true)
+                {
+                    return "jeste";
+                }
+                else
+                {
+                    return "nije";
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Kabriolet automobil" + ", marka: " + marka + ", boja: " + boja + ", broj sedista: " + brojSedista + ", dodatna oprema: " + ((DodatnaOprema == string.Empty) ? "nema" : DodatnaOprema) + ", skidajuci krov: " + GetKrovKojiSeMozeSkinuti + ", zauzima: " + zauzima + " m2, parking: " + Parking;
+        }
+    }
+}
---
+++ Fabrika/Vozila/Automobili/KlasicanAutomobil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Vozila.Automobili
+{
+    class KlasicanAutomobil : Automobil
+    {
+        private int nosivostKrova;
+
+        public KlasicanAutomobil(int nosivostKrova, int brojSedista, string marka, string boja, string[] dodatnaOprema) : base(brojSedista, marka, boja, dodatnaOprema)
+        {
+            this.nosivostKrova = nosivostKrova;
+        }
+
+        public override string ToString()
+        {
+            return "Klasican automobil" + ", marka: " + marka + ", boja: " + boja + ", broj sedista: " + brojSedista + ", dodatna oprema: " + ((DodatnaOprema == string.Empty) ? "nema" : DodatnaOprema) + ", nosivost krova: " + nosivostKrova + " kg" + ", zauzima: " + zauzima + " m2, parking: " + Parking;
+        }
+    }
+}
