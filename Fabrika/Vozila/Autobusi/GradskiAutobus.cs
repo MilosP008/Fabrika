c3b6502ac2e1b04f28591518cc3c7cd1c4509dc2
Miloš Petrović <31136523+MilosP008@users.noreply.github.com>
2019-02-08 23:41:13 +0100

    Add files via upload

---
+++ Fabrika/Vozila/Autobusi/GradskiAutobus.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Vozila.Autobusi
+{
+    class GradskiAutobus : Autobus
+    {
+        private bool zglobni;
+
+        public GradskiAutobus(bool zglobni, int brojMestaZaStajanje, int brojMestaZaSedenje, string marka, string boja) : base(brojMestaZaStajanje, brojMestaZaSedenje, marka, boja)
+        {
+            this.zglobni = zglobni;
+            zauzima = 30;
+        }
+
+        private string GetZglobni {
+            get {
+                if (zglobni == true)
+                {
+                    return "jeste";
+                }
+                else
+                {
+                    return "nije";
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Gradski autobus" + ", marka: " + marka + ", boja: " + boja + ", broj mesta za stajanje: " + brojMestaZaStajanje + ", broj mesta za sedenje: " + brojMestaZaSedenje + ", zglobni: " + GetZglobni + ", zauzima: " + zauzima + " m2, parking: " + Parking;
+        }
+    }
+}
---
+++ Fabrika/Vozila/Autobusi/MedjugradskiAutobus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Vozila.Autobusi
+{
+    class MedjugradskiAutobus : Autobus
+    {
+        private double kapacitetPrtljaznogProstora;
+
+        public MedjugradskiAutobus(double kapacitetPrtljaznogProstora, int brojMestaZaStajanje, int brojMestaZaSedenje, string marka, string boja) : base(brojMestaZaStajanje, brojMestaZaSedenje, marka, boja)
+        {
+            this.kapacitetPrtljaznogProstora = kapacitetPrtljaznogProstora;
+            zauzima = 15;
+        }
+
+        public override string ToString()
+        {
+            return "Medjugradski autobus" + ", marka: " + marka + ", boja: " + boja + ", broj mesta za stajanje: " + brojMestaZaStajanje + ", broj mesta za sedenje: " + brojMestaZaSedenje + ", kapacitet prtljaznog prostora: " + kapacitetPrtljaznogProstora + ", zauzima: " + zauzima + " m2, parking: " + Parking;
+        }
+    }
+}
