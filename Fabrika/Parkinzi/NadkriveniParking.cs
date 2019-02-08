59e32f1ad782439b7df1f9bc2ee6e50917e773eb
Miloš Petrović <31136523+MilosP008@users.noreply.github.com>
2019-02-08 23:33:51 +0100

    Add files via upload

---
+++ Fabrika/Parkinzi/NadkriveniParking.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Parkinzi
+{
+    class NadkriveniParking
+    {
+        private int povrsina;
+        private double visina;
+        private List<Vozilo> vozilaUNadkrivenomParkingu = new List<Vozilo>();
+
+        public NadkriveniParking(int povrsina, double visina) {
+            this.povrsina = povrsina;
+            this.visina = visina;
+        }
+
+        public static NadkriveniParking operator-(NadkriveniParking nadkriveniParking, Vozilo vozilo)
+        {
+            nadkriveniParking.povrsina -= vozilo.GetZauzima;
+            nadkriveniParking.vozilaUNadkrivenomParkingu.Add(vozilo);
+            return new NadkriveniParking(nadkriveniParking.povrsina, nadkriveniParking.visina);
+        }
+
+        public static NadkriveniParking operator+(NadkriveniParking nadkriveniParking, Vozilo vozilo)
+        {
+            nadkriveniParking.povrsina += vozilo.GetZauzima;
+            nadkriveniParking.vozilaUNadkrivenomParkingu.Remove(vozilo);
+            return new NadkriveniParking(nadkriveniParking.povrsina, nadkriveniParking.visina);
+        }
+
+        public int Povrsina {
+            get { return povrsina; }
+            set { if (value == 30 || value == 50) {
+                    povrsina = value;
+                }
+            }
+        }
+
+        public double Visina {
+            get { return visina; }
+            set { if (value == 2.5 || value == 5) {
+                    visina = value;
+                }
+            }
+        }
+
+        public List<Vozilo> VozilaUNadkrivenomParkingu { get { return vozilaUNadkrivenomParkingu; } }
+    }
+}
---
+++ Fabrika/Parkinzi/OtvoreniParking.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fabrika.Parkinzi
+{
+    class OtvoreniParking
+    {
+        private int povrsina;
+        private List<Vozilo> vozilaUOtvorenomParkingu = new List<Vozilo>();
+
+        public OtvoreniParking()
+        {
+            povrsina = 1000;
+        }
+
+        private OtvoreniParking(int povrsina)
+        {
+            this.povrsina = povrsina;
+        }
+
+        public static OtvoreniParking operator-(OtvoreniParking otvoreniParking, Vozilo vozilo)
+        {
+            otvoreniParking.povrsina = otvoreniParking.povrsina - vozilo.GetZauzima;
+            otvoreniParking.vozilaUOtvorenomParkingu.Add(vozilo);
+            return new OtvoreniParking(otvoreniParking.povrsina);
+        }
+
+        public static OtvoreniParking operator+(OtvoreniParking otvoreniParking, Vozilo vozilo)
+        {
+            otvoreniParking.povrsina = otvoreniParking.povrsina + vozilo.GetZauzima;
+            otvoreniParking.vozilaUOtvorenomParkingu.Remove(vozilo);
+            return new OtvoreniParking(otvoreniParking.povrsina);
+        }
+
+        public List<Vozilo> VozilaUOtvorenomParkingu{ get { return vozilaUOtvorenomParkingu; } }
+    }
+}
